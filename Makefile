16c74ad3527c181780dfbd7bf0b2898b4f68c0f9
Sandor Miskey <s@ndor.miskey.net>
2024-03-12 14:01:45 +0100

    env and init

---
+++ Makefile
@@ -0,0 +1,33 @@
+.DEFAULT_GOAL := build
+REQUIRED_VARIABLES := PATH_BASE PATH_VENV PATH_WORKBENCH
+
+all: init
+clean: dep_external
+	@gum style 'THIS TARGET CAN BE DESTRUCTIVE' 'IT SHOULD BE RUN WITH SPECIAL CARE'
+	@gum confirm "do you want to proceed?" || exit 1
+	@gum confirm "is PATH_WORKBENCH == ${PATH_WORKBENCH} correct?" || exit 1
+	@if [ ! -d ${PATH_WORKBENCH} ]; then \
+		echo "${GUM_PREFIX}${PATH_WORKBENCH} does not exist"; \
+	else \
+		gum spin --title "removing ${PATH_WORKBENCH}/*" -- find "${PATH_WORKBENCH}" -mindepth 1 -delete; \
+	fi
+	@echo "${GUM_PREFIX}${PATH_WORKBENCH} is clean"
+dep:
+	@echo "${GUM_PREFIX}checkig for env variables"
+	$(foreach var,$(REQUIRED_VARIABLES),$(if $(value $(var)),,$(error $(GUM_PREFIX) $(var) is not set, load $(ENV) before running make)))
+	@$(MAKE) dep_external
+	@$(MAKE) dep_venv
+	@echo "${GUM_PREFIX}all dependencies are inplace"
+dep_external:
+	@echo "${GUM_PREFIX}checkig for expternal dependencies"
+	@gum --version
+	@python3 --version
+dep_venv:
+	@echo "${GUM_PREFIX}checkig for python venv"
+	@${PATH_VENV}/bin/pip3 --version
+init: dep_external clean
+	@echo "${GUM_PREFIX}setting python3 venv"
+	@gum confirm "is PATH_VENV == ${PATH_VENV} correct?" || exit 1
+	@python3 -m venv ${PATH_VENV}
+	@. ${PATH_VENV}/bin/activate && ${PATH_VENV}/bin/pip3 install -U pip
+	@. ${PATH_VENV}/bin/activate && ${PATH_VENV}/bin/pip3 install -r requirements.txt
---
+++ env.sh
@@ -0,0 +1,64 @@
+#
+# .env template file, rename or symlink it, and set the appropriate values
+#
+# region: PATH
+
+export PATH_BASE=/Volumes/TExClassifier
+export PATH_WORKBENCH=${PATH_BASE}/workbench
+export PATH_VENV=${PATH_WORKBENCH}/venv
+
+# endregion
+# region: gum
+
+# generic
+export GUM_BOLD="#F0EE54"
+export GUM_NORM="#00FA92"
+export GUM_PREFIX="==> "
+
+# choose
+export GUM_CHOOSE_TIMEOUT=0
+export GUM_CHOOSE_CURSOR_FOREGROUND=$GUM_BOLD
+export GUM_CHOOSE_HEADER_FOREGROUND=$GUM_BOLD
+export GUM_CHOOSE_ITEM_FOREGROUND=$GUM_NORM
+export GUM_CHOOSE_SELECTED_FOREGROUND=$GUM_BOLD
+export GUM_CHOOSE_CURSOR=$GUM_PREFIX
+
+# confirm
+export GUM_CONFIRM_TIMEOUT=0
+export GUM_CONFIRM_PROMPT_FOREGROUND=$GUM_BOLD
+export GUM_CONFIRM_SELECTED_FOREGROUND=$GUM_NORM
+export GUM_CONFIRM_SELECTED_BACKGROUND=$GUM_BOLD
+export GUM_CONFIRM_UNSELECTED_FOREGROUND=$GUM_NORM
+
+# input
+export GUM_INPUT_CURSOR_FOREGROUND=$GUM_BOLD
+export GUM_INPUT_PROMPT_FOREGROUND=$GUM_BOLD
+export GUM_INPUT_PLACEHOLDER="your input goes here..."
+export GUM_INPUT_PROMPT=$GUM_PREFIX
+export GUM_INPUT_WIDTH=80
+
+# spin
+export GUM_SPIN_SPINNER=points
+export GUM_SPIN_SHOW_OUTPUT=true
+export GUM_SPIN_ALIGN=left
+export GUM_SPIN_SPINNER_WIDTH=$((${#GUM_PREFIX} -1 ))
+export GUM_SPIN_TIMEOUT=0
+export GUM_SPIN_SPINNER_FOREGROUND=$GUM_BOLD
+export GUM_SPIN_TITLE_FOREGROUND=$GUM_NORM
+export GUM_SPIN_SPINNER_MARGIN="0 0"
+export GUM_SPIN_SPINNER_PADDING="0 0"
+export GUM_SPIN_TITLE_MARGIN="0 0"
+export GUM_SPIN_TITLE_PADDING="0 0"
+
+# style
+export ALIGN=center
+export BOLD=true
+export BORDER=rounded
+export BORDER_FOREGROUND=$GUM_BOLD
+export FOREGROUND=$GUM_BOLD
+export MARGIN="1 2"
+export PADDING="2 4"
+export UNDERLINE=true
+export WIDTH=80
+
+# endregion: gum
